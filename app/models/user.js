cfa7e45033f7b1bdd150a72d2058f7d16eb70959
Daniel Makover <dmakover@gmail.com>
2017-03-24 12:00:15 +0300

    back end register valdation

--- app/models/user.js
+++ app/models/user.js
@@ -1,11 +1,65 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var titlize = require('mongoose-title-case');
+var validate = require('mongoose-validator');
 var bcrypt = require('bcrypt-nodejs');
 
+var nameValidator = [
+  validate({
+    validator:'matches',
+    //makes sure that allnames are in format: Firstname Lastname
+    arguments: /^(([a-zA-Z]{3,20})+[ ]+([a-zAiZ]{3,20})+)+$/,
+    message:"Name must be atleast 3 characters, max 30, no special characters, must have a space between name"
+  }),
+  validate({
+    validator:'isLength',
+    arguments:[3,25],
+    message:"name should be between {ARGS[0]} and {ARGS[1]} characters"
+  })
+];
+var emailValidator = [
+  validate({
+    validator:'isEmail',
+    //makes sure that allnames are in format: Firstname Lastname
+    message:"is not a valid email"
+  }),
+  validate({
+    validator:'isLength',
+    arguments:[3,25],
+    message:"Email should be between {ARGS[0]} and {ARGS[1]} characters"
+  })
+];
+var usernameValidator = [
+  validate({
+    validator:'isLength',
+    arguments:[3,25],
+    message:"username should be between {ARGS[0]} and {ARGS[1]} characters"
+  }),
+  validate({
+    validator:'isAlphanumeric',
+    message:"Username must contain letters and numbers ONLY"
+  })
+];
+
+var passwordValidator = [
+  validate({
+    validator:'matches',
+    //makes sure that allnames are in format: Firstname Lastname
+    arguments:/^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])(?=.*?[\W]).{8,35}$/,
+    message:"Password must containt one lowercase, one number characters, one special symbol, at least 8 characters, and no moe than 35"
+  }),
+  validate({
+    validator:'isLength',
+    arguments:[8,35],
+    message:"password should be between {ARGS[0]} and {ARGS[1]} characters"
+  })
+];
+
 var UserSchema = new Schema({
-  username:{ type: String, lowercase: true, required: true, unique: true},
-  password: { type: String, required: true},
-  email: { type: String, required: true, lowercase: true, unique: true}
+  name:{type:String, lowercase:true, required:true, validate:nameValidator},
+  username:{ type: String, lowercase: true, required: true, unique: true,validate:usernameValidator},
+  password: { type: String, required: true, validate:passwordValidator},
+  email: { type: String, required: true, lowercase: true, unique: true, validate:emailValidator}
 });
 
 UserSchema.pre('save', function(next) {
@@ -17,6 +71,10 @@ UserSchema.pre('save', function(next) {
         next();
     });
 });
+//makes all names in database lowercase for consistancy
+UserSchema.plugin(titlize,{
+  paths:['name'],
+})
 
 UserSchema.methods.comparePassword = function(password){
   return bcrypt.compareSync(password, this.password);
--- app/routes/api.js
+++ app/routes/api.js
@@ -9,17 +9,43 @@ module.exports = function(router){
     user.username = req.body.username;
     user.password = req.body.password;
     user.email = req.body.email;
+    user.name = req.body.name;
     if(req.body.username == null || req.body.username == ''
     || req.body.password == null || req.body.password == ''
-    || req.body.email == null || req.body.email == ''){
+    || req.body.email == null || req.body.email == ''
+    || req.body.name == null || req.body.name == ''){
         res.json({success:false,message: "Ensure user, email, and password were provided"})
     }else{
       user.save(function(err){
+        //If there is an Error
         if(err){
-          res.json({success:false, message:"Username or Email Already Exists!"});
-        }else{
-          res.json({success:true, message:"user created"  });
+            //if there is a validation error
+          if(err.errors != null){
+            if(err.errors.name){
+                res.json({success:false, message:err.errors.name.message});
+            }else if(err.errors.email){
+                res.json({success:false, message:err.errors.email.message});
+            }else if(err.errors.username){
+                res.json({success:false, message:err.errors.username.message});
+            }else if(err.errors.password){
+                  res.json({success:false, message:err.errors.password.message});
+            }else{
+              res.json({success:false, message:err});
+            }
+        //if there is NOT a validation error, is duplicate in database
+        }else if(err){
+          if(err.code == 11000){
+            if(err.errmsg[61] == "u"){
+              res.json({success:false, message:"Username is already taken"});
+            }else if(err.errmsg[61] == "e"){
+                res.json({success:false, message:"Email is already taken"});
+            }
+          }
         }
+      //if there is NO error
+      }else{
+        res.json({success:true, message:"user created"  });
+      }
       });
     }
   });
